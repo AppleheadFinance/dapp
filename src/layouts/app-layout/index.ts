fcaddb682209bf44952a5cb0d5c048b14113aa4d
Applehead <applehead.finance@gmail.com>
2022-02-15 19:59:25 +0200

    Update

--- src/layouts/app-layout/index.ts
+++
@@ -1,5 +0,0 @@
-//  to import from next
-import dynamic from "next/dynamic";
-//  to import from component
-export { default as Header } from "./Header";
-export const Footer = dynamic(() => import("./Footer"));
--- src/types/Resources.ts
+++
@@ -1,4 +0,0 @@
-export interface ResourcesProps {
-  title?: string;
-  path?: string;
-}
